e03851cdab3f347fd10b9e4e241c6da834c478d7
ZoeREM99 <zoerapoport290399@gmail.com>
2024-12-02 20:01:45 -0300

    add News section in Home

--- public/js/home.js
+++ public/js/home.js
@@ -1,23 +1,44 @@
+// Carrousel Principal
 $(document).ready(function(){
-    $('.single-item').slick({
+    $('.bannerPrincipal').slick({
         arrows: false,
         autoplay: true,
         autoplaySpeed:2000,
         centerMode: true,
         dots: true,
+    });
+});
+// Carrousel principal end
+
+// Carrousel News
+$(document).ready(function(){
+    $('.carrouselNews').slick({
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed:4000,
+        dots: true,  
+        slidesToShow: 5,
+        centerMode: true,
+        centerPadding: '30px',
+        slidesToScroll: 2,
         responsive: [
             {
             breakpoint: 768,
             settings: {
-                arrows: false,
-            }
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                centerMode: true,
+                centerPadding: '10px',
+              }
             },
             {
-            breakpoint: 480,
+            breakpoint: 600,
             settings: {
-                arrows: false,
-            }
+                slidesToShow: 2,
+                slidesToScroll: 1
+              }
             }
         ]
     });
 });
+// Carrousel News end
