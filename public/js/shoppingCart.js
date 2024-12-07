5c88e5e53419860da8d981277f000fc74346ca87
ZoeREM99 <zoerapoport290399@gmail.com>
2024-12-07 12:30:53 -0300

    finished shopping cart

--- public/js/shoppingCart.js
+++ public/js/shoppingCart.js
@@ -31,7 +31,7 @@ $(document).ready(function(){
             breakpoint: 600,
             settings: {
                 slidesToShow: 2,
-                slidesToScroll: 1
+                slidesToScroll: 1,
               }
             }
         ]
